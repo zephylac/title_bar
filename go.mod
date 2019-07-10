301f13b95a55af36ac6620a18d58aa1b2b7d600f
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2019-07-10 16:41:44 +0200

    WIP : https://github.com/go-flutter-desktop/go-flutter/issues/177

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/zephylac/go-flutter-plugin-titlebar/statusbar
+
+go 1.12
+
+require (
+	github.com/go-flutter-desktop/go-flutter v0.24.1
+	github.com/pkg/errors v0.8.1
+)
--- plugin.go
+++ plugin.go
@@ -1,14 +1,24 @@
-package title_bar
+package statusbar
 
-import (
-	"C"
-	"encoding/hex"
+/*
+#cgo CFLAGS: -x objective-c
+#cgo LDFLAGS: -framework Cocoa
+#import <Cocoa/Cocoa.h>
+int
+ChangeColor(int redValue, int greenValue, int blueValue, int alphaValue) {
 
-	flutter "github.com/go-flutter-desktop/go-flutter"
-	"github.com/go-flutter-desktop/go-flutter/plugin"
-)
+	id window = [[[NSApplication sharedApplication] windows] objectAtIndex:0];
 
-const channelName = "plugins.flutter.io/statusbar"
+	NSColor *myColor = [NSColor colorWithCalibratedRed:redValue green:greenValue blue:blueValue alpha:alphaValue];
+	[window setBackgroundColor: myColor];
+
+    return 0;
+}
+*/
+import "C"
+
+import flutter "github.com/go-flutter-desktop/go-flutter"
+import "github.com/go-flutter-desktop/go-flutter/plugin"
 
 // TitleBarPlugin implements flutter.Plugin and handles method calls to
 // the plugins.flutter.io/image_picker channel.
@@ -18,72 +28,13 @@ var _ flutter.Plugin = &TitleBarPlugin{} // compile-time type check
 
 // InitPlugin initializes the path provider plugin.
 func (p *TitleBarPlugin) InitPlugin(messenger plugin.BinaryMessenger) error {
-	channel := plugin.NewMethodChannel(messenger, channelName, plugin.StandardMethodCodec{})
-	channel.HandleFunc("getstatusbarcolor", p.handleGetStatusBar)
-	channel.HandleFunc("setstatusbarcolor", p.handleSetStatusBar)
-	channel.HandleFunc("setstatusbarwhiteforeground", p.handleSetStatusBarWhiteForeground)
-	channel.HandleFunc("getnavigationbarcolor", p.handleGetNavigationBarColor)
-	channel.HandleFunc("setnavigationbarwhiteforeground", p.handleSetNavigationBarWhiteForeground)
+	C.ChangeColor(1, 0, 0, 1)
 
 	return nil
 }
 
-func (p *TitleBarPlugin) handleGetStatusBar(arguments interface{}) (reply interface{}, err error) {
-	// Retrieve color
-	return nil, nil
-}
-
-func (p *TitleBarPlugin) handleSetStatusBar(arguments interface{}) (reply interface{}, err error) {
-	color := arguments.(map[interface{}]interface{})["color"].(string)
-	// animate = false
-
-	colors, _ := hex.DecodeString(color)
-	// Retrieve current window
-	//C.glfwGetCurrentContext().glClearColor(colors[0], colors[1], colors[2], colors[3])
-	//C.glfwGetCurrentContext().glClearColor(glfw.GL_COLOR_BUFFER_BIT)
-
-	return nil, nil
-}
-
-func (p *TitleBarPlugin) handleSetStatusBarWhiteForeground(arguments interface{}) (reply interface{}, err error) {
-	useWhiteForeground := arguments.(map[interface{}]interface{})["useWhiteForeground"].(bool)
-	if useWhiteForeground {
-		colors, _ := hex.DecodeString("0000000")
-		//C.glfwGetCurrentContext().glClearColor(colors[0], colors[1], colors[2], colors[3])
-		//C.glfwGetCurrentContext().glClearColor(glfw.GL_COLOR_BUFFER_BIT)
-	}
-	return nil, nil
-}
-
-func (p *TitleBarPlugin) handleGetNavigationBarColor(arguments interface{}) (reply interface{}, err error) {
-	// Get color
-	return nil, nil
-}
-
-func (p *TitleBarPlugin) handleSetNavigationBarColor(arguments interface{}) (reply interface{}, err error) {
-	color := arguments.(map[interface{}]interface{})["color"].(string)
-
-	colors, _ := hex.DecodeString(color)
-
-	// animate = false
-	// Set color & animation
-	// 'color': color.value,
-	// 'animate': animate,
-
-	//C.glfwGetCurrentContext().glClearColor(colors[0], colors[1], colors[2], colors[3])
-	//C.glfwGetCurrentContext().glClearColor(glfw.GL_COLOR_BUFFER_BIT)
-
-	return nil, nil
-}
-
-func (p *TitleBarPlugin) handleSetNavigationBarWhiteForeground(arguments interface{}) (reply interface{}, err error) {
-	// Set whitforeground navigation bar
-
-	return nil, nil
-}
-
-func (p *TitleBarPlugin) useWhiteForeground(backgroundColor string) (reply bool) {
+/* func (p *TitleBarPlugin) useWhiteForeground(backgroundColor string) (reply bool) {
 	rgba, _ := hex.DecodeString(backgroundColor)
 	// We retrieve luminance from given color
 	return 1.05/(rgba[3]+0.05) > 4.5
-}
+} */
