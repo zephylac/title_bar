cbb156281780981622406c5983cfe7f2fc1904f6
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2019-07-10 19:06:26 +0200

    Updated unsupported method

--- window_unsupported.go
+++ window_unsupported.go
@@ -1,1 +1,21 @@
+// +build !darwin
+
 package statusbar
+
+import (
+	"image/color"
+
+	"github.com/pkg/errors"
+)
+
+func setStatusBarColor(color color.RGBA) (error) {
+	return errors.New("platform unsupported")
+}
+
+func getStatusBarColor() (error) {
+	return errors.New("platform unsupported")
+}
+
+func setTitleTransparency(titleTransparency bool) (error) {
+	return errors.New("platform unsupported")
+}
